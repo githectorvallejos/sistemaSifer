f6dac52b742a3d1086fb21a1bc75d068c43f7f83
willhook2003@gmail.com <willhook2003@gmail.com>
2021-11-18 00:06:05 -0300

    turno actual falta conectar servicio back

--- sifer/src/screens/funciones-playa/turno-actual/index.js
+++ sifer/src/screens/funciones-playa/turno-actual/index.js
@@ -1,166 +1,113 @@
+import { useState } from "react";
 import {
     Container,
-    Grid,
-    Box,
-    AppBar,
-    Toolbar,
     Typography,
-    InputBase,
-    TableContainer,
-    Table,
-    TableCell,
-    TableBody,
-    TableRow,
-    TableHead,
-    IconButton,
-} from "@material-ui/core";
-  //import CheckIcon from "@material-ui/icons/Check";
-import SearchIcon from "@material-ui/icons/SearchOutlined";
+    Accordion,
+    AccordionSummary,
+    AccordionDetails,
+  } from "@material-ui/core";
+import ExpandMore from "@material-ui/icons/ExpandMore";
 
-import Theme from "theme";
-import { Delete } from "@material-ui/icons";
+
 
 const TurnoActual = () => {
 
-    const activo = [
-        {
-            nroCupon: 1001,
-            tipoTarjeta: "15/11/2021",
-            montoPago: "-",
-            formaPago: "AB123CD",
-            fechaPago: "Juan Perez"
-        },
-        {
-            nroCupon: 1002,
-            tipoTarjeta: "01/11/2021",
-            montoPago: "15/11/2021",
-            formaPago: "CD332AA",
-            fechaPago: "Jorge Mendez"
-        },
-        {
-            nroCupon: 1003,
-            tipoTarjeta: "01/10/2021",
-            montoPago: "15/10/2021",
-            formaPago: "DE221AB",
-            fechaPago: "Pedro Sanchez"
-        },
-        {
-        nroCupon: 1003,
-        tipoTarjeta: "15/09/2021",
-        montoPago: "01/10/2021",
-        formaPago: "AC123CD",
-        fechaPago: "Juan Perez"
-    }
-    ];
-    console.log("la Data", activo);
+    const [expanded, setExpanded] = useState(false);
+
+    const handleChange = (panel) => (event, isExpanded) => {
+        setExpanded(isExpanded ? panel : false);
+    };
 
 
-return (
-    <Container
-    maxWidth="lg"
-    style={{
-        marginTop: "5rem",
-        borderRadius: 10,
-        backgroundColor: "#ffffff00",
-        minHeight: "30rem",
-        paddingTop: "1rem",
-        marginBottom: "1rem",
-    }}
-    >
-    <Grid
-        container
-        item
-        style={{
-        backgroundColor: "#ffffff",
-        marginTop: "1rem",
-        boxShadow: "0px 5px 5px rgba(0, 0, 0, .2)",
-        borderRadius: 10,
-        }}
-    >
-        <Grid
-        container
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-        alignContent="center"
-        style={{
-            borderTopLeftRadius: 10,
-            borderTopRightRadius: 10,
-            backgroundColor: "#417351",
-            height: "5rem",
-        }}
-        >
-        <Box
-            sx={{ flexGrow: 1 }}
+    return (
+        <Container
+            maxWidth="lg"
             style={{
-            borderTopLeftRadius: 10,
-            borderTopRightRadius: 10,
-            height: "5rem",
-            marginTop: -65,
-            background: "transparent",
-            boxShadow: "none",
+                marginTop: "12rem",
+                borderRadius: 10,
             }}
         >
-            <AppBar
-            position="static"
-            style={{
-                borderTopLeftRadius: 10,
-                borderTopRightRadius: 10,
-                height: "5rem",
-                marginTop: 40,
-                background: "transparent",
-                boxShadow: "none",
-            }}
-            >
-            <Toolbar>
-                <Grid
-                xl={6}
-                lg={6}
-                md={6}
-                sm={6}
-                xs={6}
-                container
-                direction="column"
-                justifyContent="flex-start"
-                alignContent="flex-start"
-                style={{}}
+            <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+                <AccordionSummary
+                    expandIcon={<ExpandMore />}
+                    aria-controls="panel1bh-content"
+                    id="panel1bh-header"
                 >
-                <Typography
-                    variant="h5"
-                    style={{
-                    marginTop: "1rem",
-                    height: "3rem",
-                    color: "white",
-                    textAlign: "center",
-                    }}
+                    <Typography variant="subtitle2" style={{ width: '33%', flexShrink: 0 }}>
+                        Encargado Estación
+                    </Typography>
+                    <Typography variant="inherit" style={{ color: "text.secondary" }}>Damian Dominguez</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography variant="subtitle2" style={{ width: '33%', flexShrink: 0 }}>
+                        Hora Apertura de Turno: 07:45
+                    </Typography>
+                    <Typography variant="subtitle2" style={{ width: '33%', color: "text.secondary" }}>Turno Actual: 08:00 - 13:00 TM</Typography>
+                    <Typography variant="inherit" style={{ color: "text.secondary" }}>Sector: Oficina</Typography>
+
+                </AccordionDetails>
+            </Accordion>
+            <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+                <AccordionSummary
+                    expandIcon={<ExpandMore />}
+                    aria-controls="panel2bh-content"
+                    id="panel2bh-header"
                 >
-                    PERSONAL
-                </Typography>
-                </Grid>
-                <Grid
-                container
-                xl={6}
-                lg={6}
-                md={6}
-                sm={6}
-                xs={6}
-                direction="column"
-                justifyContent="flex-end"
-                alignContent="flex-end">
-                    
-                </Grid>
-            </Toolbar>
-            
+                    <Typography variant="subtitle2" style={{ width: '33%', flexShrink: 0 }}>
+                        Encargado Playa
+                    </Typography>
+                    <Typography variant="inherit" style={{ color: "text.secondary" }}>Victor Gonzalez</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography variant="subtitle2" style={{ width: '33%', flexShrink: 0 }}>
+                        Hora Apertura de Turno: 05:50
+                    </Typography>
+                    <Typography variant="subtitle2" style={{ width: '33%', color: "text.secondary" }}>Turno Actual: 06:00 - 14:00 TM</Typography>
+                    <Typography variant="inherit" style={{ color: "text.secondary" }}>Sector: Playa</Typography>
 
-            </AppBar>
-        </Box>
-        </Grid>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+                <AccordionSummary
+                    expandIcon={<ExpandMore />}
+                    aria-controls="panel3bh-content"
+                    id="panel3bh-header"
+                >
+                    <Typography variant="subtitle2" style={{ width: '33%', flexShrink: 0 }}>
+                        Auxiliar de Playa
+                    </Typography>
+                    <Typography variant="inherit" style={{ color: "text.secondary" }}>Osvaldo Zalazar</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography variant="subtitle2" style={{ width: '33%', flexShrink: 0 }}>
+                        Hora Apertura de Turno: 07:50
+                    </Typography>
+                    <Typography variant="subtitle2" style={{ width: '33%', color: "text.secondary" }}>Turno Actual: 08:00 - 12:00 TM</Typography>
+                    <Typography variant="inherit" style={{ color: "text.secondary" }}>Sector: Playa</Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
+                <AccordionSummary
+                    expandIcon={<ExpandMore />}
+                    aria-controls="panel4bh-content"
+                    id="panel4bh-header"
+                >
+                    <Typography variant="subtitle2" style={{ width: '33%', flexShrink: 0 }}>
+                        Seguridad
+                    </Typography>
+                    <Typography variant="inherit" style={{ color: "text.secondary" }}>Chamorro Jesus</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography variant="subtitle2" style={{ width: '33%', flexShrink: 0 }}>
+                        Hora Apertura de Turno: 05:50
+                    </Typography>
+                    <Typography variant="subtitle2" style={{ width: '33%', color: "text.secondary" }}>Turno Actual: 06:00 - 14:00 TM</Typography>
+                    <Typography variant="inherit" style={{ color: "text.secondary" }}>Sector: Vigilancia</Typography>
 
-
-        
-    </Grid>
-    </Container>
-);
+                </AccordionDetails>
+            </Accordion>
+        </Container>
+    );
 };
 
 export default TurnoActual;
